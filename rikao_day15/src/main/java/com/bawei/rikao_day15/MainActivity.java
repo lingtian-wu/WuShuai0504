047edb71078c263c4e21ecbcafe52a08cd7f8361
lingtian-wu <3557411996@qq.com>
2020-05-07 08:51:26 +0800

    提交

---
+++ rikao_day15/src/main/java/com/bawei/rikao_day15/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bawei.rikao_day15;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
